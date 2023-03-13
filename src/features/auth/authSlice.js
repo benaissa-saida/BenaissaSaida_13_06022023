6a62dbafccf3cfa7427145fb0a4c97b63c393264
benaissa-saida <benaissasaida28@gmail.com>
2023-03-13 17:32:35 +0100

    auth api call redux

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { token: null },
+  reducers: {
+    setCredentials: (state, action) => {
+      const { body } = action.payload;
+      state.token = body.token;
+    },
+    logOut: (state) => {
+      state.token = null;
+    },
+  },
+});
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const selectCurrentToken = (state) => state.auth.token;
