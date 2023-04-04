1102625aa965d21824a9f0d3dbe399534b043d80
benaissa-saida <benaissasaida28@gmail.com>
2023-04-04 11:43:08 +0200

    account data renamed

--- src/components/cards/AccountCard.jsx
+++ src/components/cards/AccountCard.jsx
@@ -1,13 +1,13 @@
 import { formatNumber } from "../../utils/formatNumber";
 
-function AccountCard({ title, amount, description }) {
+function AccountCard({ name, balance, description }) {
   return (
     <div className="account-container">
       <section className="account">
         <div className="account-content-wrapper">
-          <h3 className="account-title">{title}</h3>
-          <p className="account-amount">${formatNumber(amount)}</p>
-          <p className="account-amount-description">{description}</p>
+          <h3 className="account-title">{name}</h3>
+          <p className="account-balance">${formatNumber(balance)}</p>
+          <p className="account-balance-description">{description}</p>
         </div>
         <div className="account-content-wrapper cta">
           <button className="transaction-button">View transactions</button>
